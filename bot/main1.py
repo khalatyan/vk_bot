fd77ebf6834e6966f84b5adef9dcbb1470ec24c4
Халатян Мери <m.halatyan@agora.ru>
2025-04-06 23:37:13 +0800

    edit dockerfile

--- bot/main1.py
+++ bot/main1.py
@@ -126,6 +126,3 @@ async def callback(event: VKEvent):
         await r.expire(f"user:{user_id}", SESSION_TTL)
 
     return "ok"
-
-# if __name__ == "__main__":
-uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
