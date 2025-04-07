0bbd6184a5538b33bde9e4cbfebd452dfb76d159
Халатян Мери <m.halatyan@agora.ru>
2025-04-07 14:51:08 +0800

    commit

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,13 @@
 import re
+import os
 import vk_api
 import redis
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 # Конфигурация
-VK_TOKEN = "vk1.a.eOPbWls0EX6UZtShsBPC1-0zLPlM3jdqalpPELbR9-fugFAXSbFIakYEDf6axzK6mROKtR3tRFN_F1bhD1s2NAPlfywIQIz6GSa83rPo1uLXWB11C_vaTUr2ofMsVWCpigSE159RA4e9besVebLSRRc6U7S0Sq4_k0CRcmFSD0Ra7WonZNWopVelJYoMBlVrUoby6KF6xhfZpwniVuevPg"
-GROUP_ID = 225244314  # Укажите ID вашего сообщества
-MANAGER_IDS = [37199556, 143586526]
+VK_TOKEN = os.getenv("VK_TOKEN")
+print(VK_TOKEN)
+MANAGER_IDS = os.getenv('MANAGER_IDS').split(',')
 REDIS_HOST = "redis"
 REDIS_PORT = 6379
 SESSION_TTL = 3600  # 1 час
@@ -33,12 +34,6 @@ def validate_city(city):
 # Отправка сообщений
 def send_message(user_id, message):
     vk.messages.send(user_id=user_id, message=message, random_id=0)
-
-# Очистка просроченных сессий
-def clear_expired_sessions():
-    for key in r.keys("user:*"):
-        if r.ttl(key) == -1:
-            r.delete(key)
 
 def start_bot():
     for event in longpoll.listen():
@@ -105,12 +100,6 @@ def start_bot():
                 else:
                     send_message(user_id, "Пожалуйста, введите корректное название города.")
 
-        # elif event.type == VkEventType.GROUP_JOIN:
-        #     user_id = event.user_id
-        #     key = f"user:{user_id}"
-        #     send_message(user_id, "Привет! 😊 это бот нашего курса Рекрутер от Р до Р, твоя новая удаленная профессия. Чтобы оставить заявку, напишите слово — ДА, и наш менеджер свяжется с вами!")
-        #     r.hset(key, "stage", "wait_for_consent")
-        #     r.expire(key, SESSION_TTL)
 
 if __name__ == "__main__":
     start_bot()
